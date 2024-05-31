7b3444d76eb1ed2e8978aebe76a7a679f5bf66ff
JaagravShakalya <jaagravshakalya@gmail.com>
2024-05-31 13:05:45 +0530

    random toin coss algo implemented

--- hello.py
+++ hello.py
@@ -1,2 +1,20 @@
-for i in range(5):
-    print("hello world")
+import random
+
+p1 = 0
+p2 = 0
+
+for i in range(101):
+    number = random.randint(1,10000)
+    if number % 2:
+      p1 += 1
+    else:
+      p2 += 1
+
+print("player1 :", p1)
+print("player2 :", p2)
+
+if p2 > p1:
+   print("p2 is the winner")
+else:
+   print("p1 is the winner")
+
