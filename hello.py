e5918ea71ab85e520a315f923232f47623026200
JaagravShakalya <jaagravshakalya@gmail.com>
2024-05-31 13:38:18 +0530

    feature to play n number of times

--- hello.py
+++ hello.py
@@ -1,23 +1,34 @@
 import random
 
-p1 = 0
-p2 = 0
+def game():
+    p1 = 0
+    p2 = 0
 
-for i in range(101):
-    number = random.randint(1,10000)
-    if number % 2:
-      p1 += 1
+    for i in range(101):
+        number = random.randint(1,10000)
+        if number % 2:
+          p1 += 1
+        else:
+          p2 += 1
+
+    print("player1 :", p1)
+    print("player2 :", p2)
+
+    if p2 > p1:
+      print("p2 is the winner")
     else:
-      p2 += 1
-
-print("player1 :", p1)
-print("player2 :", p2)
-
-if p2 > p1:
-   print("p2 is the winner")
-else:
-   print("p1 is the winner")
+      print("p1 is the winner")
 
 
-print("game is over!!!!")
+    print("game is over!!!!")
+
+def main():
+   times = int(input("how many times: "))
+   for i in range(times):
+      game()
+  
+
+if __name__ == "__main__":
+    main()
+
 
